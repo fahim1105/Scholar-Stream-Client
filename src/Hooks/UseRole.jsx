6c157da884cadfc2185659fd060dab1d9404f1eb
Asif Al Fattha Fahim <asifalfatthafahim12@gmai.com>
2025-12-19 21:15:29 +0600

    add use role

---
+++ src/Hooks/UseRole.jsx
@@ -0,0 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
+import UseAuth from './UseAuth';
+import UseAxiosSecure from './UseAxiosSecure';
+
+
+const UseRole = () => {
+    const { user } = UseAuth();
+    const axiosSecure = UseAxiosSecure();
+
+    const { isLoading: roleLoading, data: role = 'user' } = useQuery({
+        queryKey: ['user-role', user.email],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/users/${user.email}/role`)
+            // console.log("In the use role", res.data)
+            return res.data?.role || 'user';
+        }
+    })
+    return { role, roleLoading };
+};
+
+export default UseRole;
