858963de676e7a7528a29dfa798fbc81d81b46d9
Asif Al Fattha Fahim <asifalfatthafahim12@gmai.com>
2026-01-12 05:18:37 +0600

    change some cimmits

--- src/Hooks/UseAxiosPublic.jsx
+++ src/Hooks/UseAxiosPublic.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: "http://localhost:3000"
-    // baseURL: "https://scholar-stream-server-alpha-fawn.vercel.app"
+    // baseURL: "http://localhost:3000"
+    baseURL: "https://scholar-stream-server-alpha-fawn.vercel.app"
 });
 
 const UseAxiosPublic = () => {
